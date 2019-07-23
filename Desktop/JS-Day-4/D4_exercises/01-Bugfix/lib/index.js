6310ff1371ae56d797cbb90d2875d4d389d2b321
Jack <superjackpower85@gmail.com>
2019-07-23 12:28:42 +0800

    initial commit

---
+++ Desktop/JS-Day-4/D4_exercises/01-Bugfix/lib/index.js
@@ -0,0 +1,5 @@
+// TODO: find the bug and fix it!
+import {displayLogo} from "./logo.js"; 
+// import "./diplayLogo"
+
+displayLogo();
---
+++ Desktop/JS-Day-4/D4_exercises/01-Bugfix/lib/logo.js
@@ -0,0 +1,5 @@
+export const displayLogo = () => {
+  const logo = document.querySelector("#logo");
+  logo.style.display = "";
+};
+
