53ce8689e7bc132ce0c12f89d538ddc065646956
Jack <superjackpower85@gmail.com>
2019-07-25 08:07:03 +0800

    geocoder

--- D4_exercises/02-Geocoder/lib/app.js
+++ D4_exercises/02-Geocoder/lib/app.js
@@ -1,29 +1,30 @@
-// // TODO: Write your JS code in here
-import mapboxgl from "mapbox-gl";
+// // // TODO: Write your JS code in here
+// import mapboxgl from "mapbox-gl";
 
-mapboxgl.accessToken = "pk.eyJ1IjoiamFjazAwNyIsImEiOiJjanlmaHRoNXAwMWIxM25xa29rYTVjbm5uIn0.nlRnwPmyOko4V6ymg6Kdpg";
+// mapboxgl.accessToken = "pk.eyJ1IjoiamFjazAwNyIsImEiOiJjanlmaHRoNXAwMWIxM25xa29rYTVjbm5uIn0.nlRnwPmyOko4V6ymg6Kdpg";
 
-var map = new mapboxgl.Map({
-  container: "map",
-  center: [-122.420679, 37.772537],
-  zoom: 13,
-  style: "mapbox://styles/mapbox/streets-v11",
-  hash: true,
-  transformRequest: (url, resourceType)=> {
-    if(resourceType === "Source" && url.startsWith("http://myHost")) {
-      return {
-        url: url.replace("http", "https"),
-        headers: { "my-custom-header": true},
-        credentials: "include"  // Include cookies for cross-origin requests
-      }
-    }
-  }
-});
+// var map = new mapboxgl.Map({
+//   container: "map",
+//   center: [-122.420679, 37.772537],
+//   zoom: 1,
+//   style: "mapbox://styles/mapbox/streets-v11",
+//   hash: true,
+//   transformRequest: (url, resourceType)=> {
+//     if(resourceType === "Source" && url.startsWith("http://myHost")) {
+//       return {
+//         url: url.replace("http", "https"),
+//         headers: { "my-custom-header": true},
+//         credentials: "include"  // Include cookies for cross-origin requests
+//       }
+//     }
+//   }
+// });
 
-// new mapboxgl.Marker()
-//   .setLngLat([ -0.077, 51.533 ])
-//   .addTo(map);
-
+// // new mapboxgl.Marker()
+// //   .setLngLat([ -0.077, 51.533 ])
+// //   .addTo(map);
+// var latEl = document.querySelector( '.latitude' );
+// var longEl = document.querySelector( '.longitude' );
 // var api = "pk.eyJ1IjoiamFjazAwNyIsImEiOiJjanlmaHRoNXAwMWIxM25xa29rYTVjbm5uIn0.nlRnwPmyOko4V6ymg6Kdpg";
 
 // var userInput = document.querySelector("#input");
@@ -45,34 +46,4 @@ var map = new mapboxgl.Map({
 //     });
 // });
 
-// var https = require('https');
-
-// function geocode(mapboxAccessToken, query, callback) {
-//     https.get('https://api.tiles.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(query) + '.json?access_token=' + mapboxAccessToken,
-//         function(response) {
-//             var body = '';
-//             response.on('data', function(d) {
-//                 body += d;
-//             });
-//             response.on('error', function(e) {
-//                 callback(e);
-//             });
-//             response.on('end', function() {
-//                 callback(null, JSON.parse(body));
-//             });
-//         });
-// }
-
-// if (require.main === module) {
-//     if (!process.env.MapboxAccessToken) {
-//         console.log('environment variable MapboxAccessToken must be set');
-//         process.exit(1);
-//     }
-//     geocode(process.env.MapboxAccessToken, process.argv[2], function(err, result) {
-//         if (err) return console.log('Error: ' + err);
-//         console.log(JSON.stringify(result, null, 2));
-//     });
-// }
-
-// module.exports = geocode;
 
